3da2527d3d20de1e3397a0adf398b75f811740d0
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-11-24 21:26:16 +0500

    update the solidity version

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,9 +1,7 @@
 // I'm a comment!
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.8;
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
+pragma solidity 0.8.17;
 
 contract SimpleStorage {
 
